47c753919a07a87188169296892809da86da68aa
Taph-tech <mustaphac687@gmail.com>
2025-12-10 02:12:59 -0500

    Post ID Urls added

--- 3-3-url-parameters/starter/app.py
+++ 3-3-url-parameters/starter/app.py
@@ -11,10 +11,13 @@ posts = {
 def home():
     return '<h1>Welcome to My Blog</h1><p>Click on the posts to learn more about Flask.</p>'
 
-@app.route('/post/1')
-def show_post():
-    post = posts[1]
-    return f"<h1>{post['title']}</h1><p>{post['content']}</p>"
+@app.route('/post/<int:post_id>') # dynamic route to capture post_id from the URL
+def show_post(post_id):
+    post = posts.get(post_id) # retrieve the post based on the post_id from the URL
+    if not post:
+        return '<h1>404: Post not found</h1>', # if post does not exist return a 404 error message
+    return f"<h1>{post['title']}</h1><p>{post['content']}</p>" # if post exists dynamically display an 
+                                                                # HTML page with the post title and content
 
 if __name__ == '__main__':
     app.run(debug=True)
