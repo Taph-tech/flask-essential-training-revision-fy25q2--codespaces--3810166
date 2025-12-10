e7f903a59fc9323b42b4d4b69e68ed71900de87d
Taph-tech <mustaphac687@gmail.com>
2025-12-10 02:49:18 -0500

    Form and Jinja templates implemented

--- 3-4-project-home-page-route/starter/app.py
+++ 3-4-project-home-page-route/starter/app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/form', methods=['GET', 'POST']) # Route to handle form display and submission
+def form():
+      if request.method == 'POST':  # Check if the form is submitted posted
+          # Process form data here
+          return redirect(url_for('dashboard')) # Redirect to dashboard after form submission
+                                                # dashboard should not have dashboard.html
+      return render_template('form.html') # Render the form template for GET requests
+  
+@app.route('/dashboard') # Dashboard route to display after form submission
+def dashboard():
+    return render_template('dashboard.html') # Render the dashboard template
 
 if __name__ == '__main__':
     app.run(debug=True)
--- 4-1-intro-jinja-templating/starter/app.py
+++ 4-1-intro-jinja-templating/starter/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    user = {'username': 'Mustapha'} # Example user data
+    return render_template('index.html', title = 'Home', user = user) # Render the index.html template with user data
 
 if __name__ == '__main__':
     app.run(debug=True)
