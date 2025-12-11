c8a63db8739db328a002236722efbcc14cf99054
Taph-tech <mustaphac687@gmail.com>
2025-12-11 00:58:24 -0500

    Flask_wtf(forms) added in file

--- 5-1-form-creation/starter/app.py
+++ 5-1-form-creation/starter/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.secret_key = 'supersecterkey'
 
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    form = MyForm()
+    if form.validate_on_submit():
+     return f'Hello, {form.name.data}!'
+    return render_template('form.html', form=form)
 
 
 if __name__ == '__main__':
