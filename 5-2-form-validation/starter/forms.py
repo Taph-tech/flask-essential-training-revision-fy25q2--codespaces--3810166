971786cc8d0263ba8f07bcbcc395dff9a6399281
Taph-tech <mustaphac687@gmail.com>
2025-12-11 14:20:15 -0500

    Form validation and form submission added from 5-2 to 5-3

--- 5-2-form-validation/starter/forms.py
+++ 5-2-form-validation/starter/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, email 
 
 class MyForm(FlaskForm):
-    username = StringField('Username')
+    username = StringField('Username', validators = [DataRequired()])
+    email = StringField('Email', validators=[email()])
     submit = SubmitField('Submit')
