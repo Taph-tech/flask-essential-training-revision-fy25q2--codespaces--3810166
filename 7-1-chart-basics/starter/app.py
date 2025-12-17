f09e9c2540e42e3f01e2db133dc24317b46b0612
Taph-tech <mustaphac687@gmail.com>
2025-12-17 01:49:35 -0500

    Create, read, delete and database set up added.Also the chart.js implemented for the barchat to show

--- 7-1-chart-basics/starter/app.py
+++ 7-1-chart-basics/starter/app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to the Flask Chart.js Example!"
+    return "Welcome to the Flask Chart.js Example!" 
 
+
+@app.route('/chart')
+def chart():
+    return render_template('chart.html')    
 if __name__ == '__main__':
     app.run(debug=True)
